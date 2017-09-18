5440caf6a134648dbef0340b11e1612e0ed0998c
SRAVAN\sravan sunny <rakhee225@gmail.com>
2017-09-18 08:06:27 +0530

    Modifications

--- VC/layout/layout.js
+++ VC/layout/layout.js
@@ -1,7 +1,5 @@
 angular.module("OR").controller("LayoutController",["$scope","$mdSidenav",function($scope,$mdSidenav) {
-
     $scope.open = function() {
-        console.log("OPEN");
         $mdSidenav('left').toggle();
     };
 }]);
